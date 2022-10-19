c49321a352127b6c21643ac74a8585e7b0f295b0
Leonardo Vilches <leonardo.vilches@vegasolutions.com.br>
2022-10-18 22:00:54 -0300

    add project

---
+++ src/test/java/com/lv08/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lv08.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
